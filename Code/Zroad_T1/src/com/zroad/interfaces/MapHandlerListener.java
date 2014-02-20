c2bf2e1f2d49a7575b35a8d0d51391b47c4b5514
vicxlau <vicxlau@gmail.com>
2014-02-20 12:54:35 +0800

    add icon samples

---
+++ Code/Zroad_T1/src/com/zroad/interfaces/MapHandlerListener.java
@@ -0,0 +1,7 @@
+package com.zroad.interfaces;
+
+import com.google.android.gms.maps.GoogleMap;
+
+public interface MapHandlerListener {
+		public void onMapHlrLocationChanged(GoogleMap result);
+}
---
+++ Code/Zroad_T1/src/com/zroad/utils/ArchitectViewController.java
@@ -0,0 +1,15 @@
+package com.zroad.utils;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class ArchitectViewController {
+	private LatLng cur;
+	private LatLng dest;
+
+	public ArchitectViewController(LatLng c,LatLng d) {
+		cur=c;
+		dest=d;
+	}
+
+
+}

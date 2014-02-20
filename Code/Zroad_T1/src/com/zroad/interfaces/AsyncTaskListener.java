b62d2aced22a22268cd16bebf11cbbb4713f4e2d
vicxlau <vicxlau@gmail.com>
2014-02-20 21:58:16 +0800

    - change abstract methods @AsyncTaskListener

--- Code/Zroad_T1/src/com/zroad/interfaces/AsyncTaskListener.java
+++ Code/Zroad_T1/src/com/zroad/interfaces/AsyncTaskListener.java
@@ -1,7 +1,12 @@
 package com.zroad.interfaces;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.google.android.gms.maps.GoogleMap;
 
 public interface AsyncTaskListener {
 	public void onTaskComplete(ArrayList<Double> result);
+	public void onTaskComplete(List<List<HashMap<String,String>>> result);
 }

305150d1f3121011039fbe303b8c439172cc864c
vicxlau <vicxlau@gmail.com>
2014-03-04 22:36:42 +0800

    - pass List<String>instructions through AsyncTaskListener

--- Code/Zroad_T1/src/com/zroad/interfaces/AsyncTaskListener.java
+++ Code/Zroad_T1/src/com/zroad/interfaces/AsyncTaskListener.java
@@ -1,12 +1,9 @@
 package com.zroad.interfaces;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import com.google.android.gms.maps.GoogleMap;
-
 public interface AsyncTaskListener {
-	public void onTaskComplete(ArrayList<Double> result);
-	public void onTaskComplete(List<List<HashMap<String,String>>> result);
+	public void onTaskComplete(List<Double> result);
+	public void onTaskComplete(List<List<HashMap<String,String>>> result,List<String>instructions);
 }

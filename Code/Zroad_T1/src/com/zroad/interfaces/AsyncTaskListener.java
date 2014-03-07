92624ef89f354e7879db0d23d84f16576e80bd82
vicxlau <vicxlau@gmail.com>
2014-03-07 18:23:58 +0800

    - add durations, google_warning , change List<double> to LatLng @AsyncTaskListener,SDA,MHlr,RIC

--- Code/Zroad_T1/src/com/zroad/interfaces/AsyncTaskListener.java
+++ Code/Zroad_T1/src/com/zroad/interfaces/AsyncTaskListener.java
@@ -2,8 +2,10 @@ package com.zroad.interfaces;
 
 import java.util.HashMap;
 import java.util.List;
+import com.google.android.gms.maps.model.LatLng;
 
 public interface AsyncTaskListener {
-	public void onTaskComplete(List<Double> result);
-	public void onTaskComplete(List<List<HashMap<String,String>>> result,List<String>instructions);
+	public void onTaskComplete(LatLng result);
+	public void onTaskComplete(String result);
+	public void onTaskComplete(List<List<HashMap<String,String>>> result,List<String>instructions,List<String>durations,String google_warning);
 }
